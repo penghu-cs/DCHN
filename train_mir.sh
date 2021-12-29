9422518fbe580d3f2d5add5fa9d7659a5603485d
penghu-cs <45786915+penghu-cs@users.noreply.github.com>
2021-12-29 10:00:37 +0800

    Update train_mir.sh

--- train_mir.sh
+++ train_mir.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-# train SHAM
 available_num=100
 output_shape=64
 gama=1
@@ -10,13 +9,17 @@ gama=1
 seed=0
 epochs=100
 
+# train SHAM
 python trainSHAM.py --datasets $datasets --output_shape $output_shape --gama $gama --available_num $available_num
+
+# train image
+python main_DCHN.py --seed $seed --epochs $epochs --view 0 --datasets $datasets --output_shape $output_shape --alpha $alpha --gama $gama --available_num $available_num --gpu_id 0 &
+
 # train Text
 python main_DCHN.py --seed $seed --epochs $epochs --view 1 --datasets $datasets --output_shape $output_shape --alpha $alpha --gama $gama --available_num $available_num --gpu_id 1 &
-# train image
-python main_DCHN.py --seed $seed --epochs $epochs --view 0 --datasets $datasets --output_shape $output_shape --alpha $alpha --gama $gama --available_num $available_num --gpu_id 0
 
 # python main_DCHN.py --view -1 --datasets $datasets --output_shape $output_shape --alpha $alpha --gama $gama --available_num $available_num --gpu_id -1 --multiprocessing
 
+wait
 # eval
 python main_DCHN.py --epochs $epochs --view -1 --datasets $datasets --output_shape $output_shape --alpha $alpha --gama $gama --available_num $available_num --gpu_id 0 --mode eval --num_workers 0
